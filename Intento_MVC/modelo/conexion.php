9b40a747e39f9e4f67dd2669d5547a6f02b22beb
Martín <mexacraftt200@gmail.com>
2022-08-22 15:54:21 -0300

    Agregado de archivo de conexion al intento de MVC

---
+++ Intento_MVC/modelo/conexion.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conectar{
+    public static function conexion (){
+    
+        try{
+    
+            $host ="localhost";
+            $user ="root";
+            $pass="";
+            $bd="burger_factory";
+            $con = mysqli_connect($host,$user,$pass);
+            mysqli_select_db($con,$bd);
+            return $con;
+    
+        }catch(Exception $e){
+    die('Error'.$e->getMessage());
+    echo "La linea de error " . $e->getLine();
+        }
+    }
+    
+    
+    }
+    
+    
+    
+    
+    ?>
