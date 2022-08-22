af815c8f688f8fa8932f212b18f955dc8400050c
Martín <mexacraftt200@gmail.com>
2022-08-22 16:23:42 -0300

    Update index.php

--- Intento_MVC/index.php
+++ Intento_MVC/index.php
@@ -4,8 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="iniciostyle.css">
   <title>Inicio de Sesión</title>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link rel="stylesheet" href="iniciostyle.css">
 </head>
 <body>
     <form method="POST" action="procesarSesion.php">
