f21f3fdb2354a7002b639f17a12e3636062ca6d0
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-08-24 08:35:48 -0300

    Update Modelo_InicioSesion.php

--- Intento_MVC/modelo/Modelo_InicioSesion.php
+++ Intento_MVC/modelo/Modelo_InicioSesion.php
@@ -9,7 +9,7 @@ $habemus_correo=null;
 $habemus_contraseña=null;
 
 if($correo!=null && $contraseña!=null){
-    $query_correo = "SELECT * FROM datos WHERE correo = '$correo'";
+    $query_correo = "SELECT * FROM usuarios WHERE correo = '$correo'";
     $correo_captado = mysqli_query($connection, $query_correo);
     $habemus_correo = mysqli_fetch_array($correo_captado);
     if($habemus_correo){
@@ -17,24 +17,17 @@ if($correo!=null && $contraseña!=null){
         $nombre = $habemus_correo['nombre'];
         $apellidos = $habemus_correo['apellido'];
         }
-    $query_contraseña = "SELECT * FROM datos WHERE password = '$contraseña' AND id = '$id'";
+    $query_contraseña = "SELECT * FROM usuarios WHERE password = '$contraseña' AND id = '$id'";
     $contraseña_captado = mysqli_query($connection, $query_contraseña);
     $habemus_contraseña = mysqli_num_rows($contraseña_captado);
     }else{
         header("Location: inicioFallido.php");
     }
 
-    if($habemus_correo){
-    ?><h4>Sesión iniciada con exito!</h4><?php
+    if($habemus_correo && $habemus_contraseña){
+        header("Location: ../index.php");
     }else{
-        header("Location: inicioFallido.php");
+        header("Location: ../.php");
     }
-    if(!$habemus_contraseña){
-    header("Location: inicioFallido.php");
-    }
-    print "Bienvenido $nombre $apellidos!";
-    ?>
-    <br>Funcionanding.
-    </section>
 </body>
 </html>
