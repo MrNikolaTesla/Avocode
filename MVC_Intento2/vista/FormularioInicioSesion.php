38beb9eb99dd1727ade346d85efc287b24eee869
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-08-24 12:02:50 -0300

    Update FormularioInicioSesion.php

--- MVC_Intento2/vista/FormularioInicioSesion.php
+++ MVC_Intento2/vista/FormularioInicioSesion.php
@@ -16,7 +16,8 @@
             <?php require_once("../controlador/ControladorInicioSesion.php"); ?>
                 <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo"></input>
                 <input class="controls" type="password" name="password" id="password" placeholder="Ingrese su Contraseña"></input>
-                <input class="buttons" type="submit" name="boton_inicio" value="Registrar"></input>
+                <input class="buttons" type="submit" name="boton_inicio" value="Iniciar Sesion"></input>
+                <p><a href="FormularioRegistroCliente.php">Registrarse</a></p>
         </section>
     </form>
 
