cdddff5b11537121844d5c38624da5c66a36fa41
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-08-24 10:07:43 -0300

    Add files via upload

---
+++ Crud_Ejemplo/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+        <h1 class="text-center">CRUD COMPLETO</h1>
+        <div class="container-fluid row">
+                
+                <!-- INICIO FORMULARIO ALTA -->
+                <form class="col-4" method="POST">
+                  <h3 class="text-center">Registro de personas</h3>
+                  <?php require_once("controlador/RegistroAlumno_controlador.php"); ?>
+                  <div class="mb-3">
+                    <label class="form-label">Nombre de la persona</label>
+                    <input type="text" class="form-control" name="nombre">
+                   </div>
+                   <div class="mb-3">
+                    <label class="form-label">Apellido de la persona</label>
+                    <input type="text" class="form-control" name="apellido">
+                   </div>
+                   <div class="mb-3">
+                    <label class="form-label">Edad de la persona</label>
+                    <input type="text" class="form-control" name="edad">
+                   </div>
+                   <div class="mb-3">
+                    <label class="form-label">Nota de la persona</label>
+                    <input type="text" class="form-control" name="nota">
+                   </div>
+                  <button type="submit" class="btn btn-primary" name="btnenviar" value="enviar">Enviar</button>
+                </form>
+                <!-- FIN FORMULARIO ALTA -->
+        
+                <div class="col-8 ">
+                        <!-- INICIO TABLA LISTAR -->
+                        <?php require_once("controlador/Alumnos_controlador.php"); ?>
+                        <!-- FIN TABLA LISTAR -->
+                </div>
+        </div>
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+</body>
+</html>
