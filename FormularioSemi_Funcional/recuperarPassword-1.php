a321575423e7ec7f2cc7a003a91f3f51ae602dde
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-08-24 08:20:18 -0300

    Add files via upload

---
+++ FormularioSemi_Funcional/recuperarPassword-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>Recuperar Contraseña</title>
+</head>
+<body>
+
+  <section class="form-register">
+Algo me dice que esto no va xD
+  </section>
+</body>
+</html>
