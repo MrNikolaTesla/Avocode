d242c88c237d87bc510a5d454c50b584525ec798
Lautaro <110629515+MrNikolaTesla@users.noreply.github.com>
2022-08-24 11:50:57 -0300

    CORRECCIÓN FINAL DE FICHEROS, CÓDIGO Y CSS

--- NO-MVC/procesarRegistro.php
+++ NO-MVC/procesarRegistro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>Document</title>
 </head>
 <body>
@@ -15,7 +15,7 @@ include("conexionBD.php");
 $POSTnombre=$_POST['nombre'];
 $POSTapellido=$_POST['apellido'];
 $POSTcorreo=$_POST['correo'];
-$POSTpassword=$_POST['passwordRegistro1'];
+$POSTpassword=$_POST['password'];
 
 $query = "INSERT INTO datos(nombre, apellido, correo, password) VALUES (nombre, apellido, correo, password)";
 $result = mysqli_query($connection, $query);
--- NO-MVC/terminos.php
+++ NO-MVC/terminos.php
@@ -4,14 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="styles/styles.css">
   <title>Formulario Registro</title>
 </head>
 
 <body>
-     <div class="div-regular"> Los Términos y Condiciones representan el documento que ayuda a prevenir y resolver los problemas.
+     <div class="div-regular"><p>Los Términos y Condiciones representan el documento que ayuda a prevenir y resolver los problemas.
     Por ello, son fundamentales en muchos casos para defenderse en caso de abuso.
-    Las Condiciones de servicio establecen la forma en que se puede utilizar tu producto, servicio o contenido de forma legalmente vinculante.</div>
+    Las Condiciones de servicio establecen la forma en que se puede utilizar tu producto, servicio o contenido de forma legalmente vinculante.</p></div>
 </body>
 
 </html>
