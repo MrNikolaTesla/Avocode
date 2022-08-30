3b97c354bcfc31b6c6a397278c6f16e1949f9cb9
Martín <mexacraftt200@gmail.com>
2022-08-30 17:17:18 -0300

    Sufriendo con el Modificar

--- MVC_Intento2/controlador/ControladorModificarUsuario.php
+++ MVC_Intento2/controlador/ControladorModificarUsuario.php
@@ -1,7 +1,7 @@
 <?php
     require_once("../modelo/ActualizarUsuario.php");
     $actualizarUsuario = new ActualizarUsuario_modelo();
-if(!empty($_POST["modificar"])){
+if(!empty($_POST["modificacion"])){
     if(!empty($_POST["nombre"]) and !empty($_POST["apellido"]) and !empty($_POST["correo"]) and !empty($_POST["password"]) and !($_POST["tipo"] == "null")) {
         
         //echo "<div class="alert alert-success">Alumno dado de alta correctamente</div>";
@@ -15,13 +15,16 @@ if(!empty($_POST["modificar"])){
         $telefono = $_POST["telefono"];
         $tipo = $_POST["tipo"];
 
-        $estado = $actualizarUsuario->update_usuario($nombre, $apellido, $correo, $password, $direccion, $telefono, $tipo);
+        $estado = $actualizarUsuario->update_usuario($id, $nombre, $apellido, $correo, $password, $direccion, $telefono, $tipo);
 
 if($estado==1) {
+
     echo '<div class="alert alert-success">Usuario modificado correctamente!</div>';
 }else{
+    echo '<div class="alert alert-danger">El otro algo ha ocurrido.</div>';
+}
+}
+}else{
     echo '<div class="alert alert-danger">Algo ha ocurrido.</div>';
-}
-}
 }
 ?>
