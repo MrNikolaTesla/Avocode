cc10af17034119f80fc16ace3e35b6610389a288
Lautaro <galeazzil2021@gmail.com>
2022-08-31 23:38:59 -0300

    TESTEO DE CSS

--- MVC_Intento2/controlador/MenuPrincipal_VerificacionPermisos.php
+++ MVC_Intento2/controlador/MenuPrincipal_VerificacionPermisos.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
 <?php
 session_start(); 
 $permiso = $_SESSION['tipo'];
@@ -7,16 +17,29 @@ if($permiso == "cliente"){
    echo "<li style='float:left'><a href=''>2OpcionCliente</a></li>";
    echo "<li style='float:left'><a href=''>3OpcionCliente</a></li>";
    echo "<li style='float:left'><a href=''>4OpcionCliente</a></li>";
-}else if ($permiso == "empleado"){
+
+    
+
+
+ }else if ($permiso == "empleado"){
     echo "<li style='float:left'><a href=''>1OpcionEmpleado</a></li>";
     echo "<li style='float:left'><a href=''>2OpcionEmpleado</a></li>";
     echo "<li style='float:left'><a href=''>3OpcionEmpleado</a></li>";
     echo "<li style='float:left'><a href=''>4OpcionEmpleado</a></li>";
 }else{
-    echo "<li style='float:left'><a href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li>";
+    /* echo "<li style='float:left'><a href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li>";
     echo "<li style='float:left'><a href='GestionDeProductos.php'>Gestor de Productos</a></li>";
     echo "<li style='float:left'><a href=''>3OpcionAdministrador</a></li>";
-    echo "<li style='float:left'><a href=''>4OpcionAdministrador</a></li>";
+    echo "<li style='float:left'><a href=''>4OpcionAdministrador</a></li>"; */
+
+    echo '<button class="button-84" role="button"><a href="GestionDeUsuarios.php">Gestor de Usuarios</a></button>';
+    echo '<button class="button-84" role="button"><a href="GestionDeProductos.php">Gestor de Productos</a></button>';
+    echo '<button class="button-84" role="button"><a href="">3OpcionAdministrador</a></button>';
+    echo '<button class="button-84" role="button"><a href="">4OpcionAdministrador</a></button>';
+
 }
 
-?>
+?>    
+</body>
+</html>
+
--- MVC_Intento2/pagina/menu_principal.php
+++ MVC_Intento2/pagina/menu_principal.php
@@ -1,51 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/styles.css">
-    <title>Menu Principal</title>
-    <style>
-ul {
-  list-style-type: none;
-  font-family: sans-serif;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link rel="stylesheet" href="../assets/styles2.css">
+  <title>Menu Principal</title>
 
-li {
-  float: left;
-  border-right:1px solid #bbb;
-}
-
-li:last-child {
-  border-right: none;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li a:hover:not(.active) {
-  background-color: #111;
-}
-
-.active {
-  background-color: #3F3328;
-}
-</style>
 </head>
+
 <body>
-<ul>
-<?php require_once("../controlador/MenuPrincipal_VerificacionPermisos.php"); ?>
-</ul>
+  <nav>
+    <?php require_once("../controlador/MenuPrincipal_VerificacionPermisos.php"); ?>
+  
+</nav>
+
 </body>
+
 </html>
