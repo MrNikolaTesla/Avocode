4090fb5a3060b06152311484930a534b9d29ca75
Martín <mexacraftt200@gmail.com>
2022-09-02 14:53:40 -0300

    Estructura BD Boceto
    Voy a hacer datos de prueba

--- MVC_Intento2/assets/Base de Datos/Grupo2_estructura.sql
+++ MVC_Intento2/assets/Base de Datos/Grupo2_estructura.sql
@@ -1,15 +1,17 @@
-﻿
+﻿--Tuvimos grandes cantidades de problemas con las FK, principalmente a la hora de agregarlas y exportarlas, por lo tanto, las mismas estan faltantes es este boceto de BD
+--Cabe aclarar que si sabemos que atributos volver FK (Principalmente cliente y empleado), los hubieramos colocado pero tuvimos 4000 errores, no teniamos dia para preguntar e internet no logro proporcionarnos una respuesta
+
 CREATE TABLE `articulo` (
-  `id_articulo` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID del Producto',
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID del Producto',
   `nombre` varchar(30) NOT NULL COMMENT 'Nombre del Producto',
   `precio` int(20) NOT NULL COMMENT 'Precio del Producto',
-  `tipo` varchar(15) NOT NULL COMMENT 'Tipo de producto (vegano, vegetariano, celíaco,  etc).'
+  `tipo` varchar(15) NOT NULL COMMENT 'Tipo de producto (vegano, vegetariano, celiaco,  etc).'
 );
 
 CREATE TABLE `factura` (
-  `id_factura` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID de Factura',
-  `cliente` varchar(40) NOT NULL COMMENT 'Cliente al que le corresponde la factura', --Esto seria FK
-`empleado` varchar(40) NOT NULL COMMENT 'Empleado que atendió al cliente', --Esto seria FK
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID de Factura',
+  `cliente` varchar(40) NOT NULL COMMENT 'Cliente al que le corresponde la factura',
+`empleado` varchar(40) NOT NULL COMMENT 'Empleado que atendió al cliente',
   `fecha` date NOT NULL COMMENT 'Fecha de la factura',
   `hora` varchar(10) NOT NULL COMMENT 'Hora en la que se realizó la factura',
   `forma_pago` enum('Credito','Debito','Online','Casa_Credito') NOT NULL COMMENT 'Forma de pago utilizada para la factura',
@@ -18,22 +20,23 @@ CREATE TABLE `factura` (
 );
 
 CREATE TABLE `mesa` (
-  `id_mesa` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID de la Mesa',
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID de la Mesa',
   `estado` enum('Libre','Ocupada','Reservada') NOT NULL COMMENT 'Estado de la Mesa'
 );
 
 CREATE TABLE `orden` (
-  `id_orden` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID de la orden',
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID de la orden', --Se crearan FK cliente y empleado para verificar e origen y direccion de la orden
   `fecha` date NOT NULL COMMENT 'Fecha de la orden realizada',
   `observacion` varchar(90) NOT NULL COMMENT 'Comentarios para realizar el pedido',
   `hora` varchar(10) NOT NULL COMMENT 'Hora de la orden',
-  `articulos` varchar(100) NOT NULL COMMENT 'Articulos de la orden', --Este trozo de aca va a cambiar con una relacion a una entidad nueva que tenemos que pensar mejor
-  -- para no crear redundancia a la hora de poner multiples productos relacionados a la misma orden
+  `articulos` varchar(100) NOT NULL COMMENT 'Articulos de la orden', --Tanto articulos como precio seran cambiados por una relacion con una posible
+  --tabla a nombre de lineas, permitiendo evitar la redundancia de multiples productos dentro de una misma comanda
   `precio` int(10) NOT NULL COMMENT 'Precio de la orden'
 );
 
-CREATE TABLE `proveedor` ( --Esta tabla solo va a ser un CRUD, no se relacionara con nada mas
-  `id_proveedor` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID del Proveedor',
+--Esta tabla es solo un archivador de datos, no se relacionara con ninguna de las otras
+CREATE TABLE `proveedor` (
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID del Proveedor',
   `nombre_apellido` varchar(60) NOT NULL COMMENT 'Nombre y Apellido del Proveedor',
   `empresa` varchar(40) NOT NULL COMMENT 'Empresa a la que pertenece el Proveedor',
   `productos` varchar(30) NOT NULL COMMENT 'Productos que proporciona el Proveedor',
@@ -41,17 +44,17 @@ CREATE TABLE `proveedor` ( --Esta tabla solo va a ser un CRUD, no se relacionara
 );
 
 CREATE TABLE `reserva` (
-  `id_reserva` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID de Mesa',
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID de Mesa',
   `mesa` int(10) NOT NULL COMMENT 'Numero de Mesa',
   `fecha` date NOT NULL COMMENT 'Fecha en la que se ocupo la Mesa',
   `hora` int(10) NOT NULL COMMENT 'Hora en que se ocupo la Mesa',
-  `cliente` int(40) NOT NULL COMMENT 'Cliente que ocupo la Mesa', --Esto seria FK
-  `empleado` int(40) NOT NULL COMMENT 'Empleado que atendio la Mesa' --Esto seria FK
+  `cliente` int(40) NOT NULL COMMENT 'Cliente que ocupo la Mesa',
+  `empleado` int(40) NOT NULL COMMENT 'Empleado que atendio la Mesa'
 );
 
+--Muy probablemente tengamos que hacer una nueva entidad a nombre de empleados para las FK a futuro
 CREATE TABLE `usuario` (
-  `id_usuario` int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID del Usuario', --Aca probablemente no tengamos que crear una entidad empleado y relacionarla con usuario, principalmente
-  --a la hora de hacer las FK de arriba
+  `id` int(10) PRIMARY KEY NOT NULL COMMENT 'ID del Usuario',
   `nombre` varchar(40) NOT NULL COMMENT 'Nombre del Usuario',
   `apellido` varchar(40) NOT NULL COMMENT 'Apellido del Usuario',
   `correo` varchar(60) NOT NULL COMMENT 'Correo del Usuario',
