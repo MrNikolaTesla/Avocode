91d8d6514407c53ad5b5086209bb2d6ef218c15d
Tabare8031 <facundovuerra@gmail.com>
2022-09-07 17:50:59 -0300

    Estilo en la página 'menu_principal'
    Aún muy básico, me apegue a lo que tenemos en el mockup

--- PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
+++ PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
@@ -1,45 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Document</title>
-</head>
-<body>
 <?php
 session_start(); 
 $permiso = $_SESSION['tipo'];
 
 if($permiso == "cliente"){
-   echo "<li style='float:left'><a href=''>1OpcionCliente</a></li>";
-   echo "<li style='float:left'><a href=''>2OpcionCliente</a></li>";
-   echo "<li style='float:left'><a href=''>3OpcionCliente</a></li>";
-   echo "<li style='float:left'><a href=''>4OpcionCliente</a></li>";
-
+   echo "<li><a class='boton' href=''>1OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
+   echo "<li><a class='boton' href=''>2OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
+   echo "<li><a class='boton' href=''>3OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
+   echo "<li><a class='boton' href=''>4OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
     
 
 
  }else if ($permiso == "empleado"){
-    echo "<li style='float:left'><a href=''>1OpcionEmpleado</a></li>";
-    echo "<li style='float:left'><a href=''>2OpcionEmpleado</a></li>";
-    echo "<li style='float:left'><a href=''>3OpcionEmpleado</a></li>";
-    echo "<li style='float:left'><a href=''>4OpcionEmpleado</a></li>";
+    echo "<li><a class='boton' href=''>1OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href=''>2OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href=''>3OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href=''>4OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
 }else{
     /* echo "<li style='float:left'><a href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li>";
     echo "<li style='float:left'><a href='GestionDeProductos.php'>Gestor de Productos</a></li>";
     echo "<li style='float:left'><a href=''>3OpcionAdministrador</a></li>";
     echo "<li style='float:left'><a href=''>4OpcionAdministrador</a></li>"; */
 
-    echo '<button class="button-84" role="button"><a href="GestionDeUsuarios.php">Gestor de Usuarios</a></button>';
-    echo '<button class="button-84" role="button"><a href="GestionDeProductos.php">Gestor de Productos</a></button>';
-    echo '<button class="button-84" role="button"><a href="">3OpcionAdministrador</a></button>';
-    echo '<button class="button-84" role="button"><a href="">4OpcionAdministrador</a></button>';
-
+    echo "<li><a class='boton' href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href='GestionDeProductos.php'>Gestor de Productos</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href=''>3OpcionAdministrador</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href=''>4OpcionAdministrador</a></li><hr size=5 noshade='noshade' color='#000'>";
 }
-
-?>    
-</body>
-</html>
-
--- PaginaGrupo2/pagina/menu_principal.php
+++ PaginaGrupo2/pagina/menu_principal.php
@@ -6,17 +6,17 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <link rel="stylesheet" href="../assets/styles2.css">
+  <link rel="stylesheet" type="text/css" href="../assets/style_menu_principal.css">
   <title>Menu Principal</title>
 
 </head>
 
 <body>
-  <nav>
+  <div class="menuNav">
+  	<ul>
     <?php require_once("../controlador/MenuPrincipal_VerificacionPermisos.php"); ?>
-  
-</nav>
-
+    </ul>
+</div>
 </body>
 
 </html>
