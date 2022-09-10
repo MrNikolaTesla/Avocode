4da2df6266716fcd708915bd0da7ecc4d2cd2ddd
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-09-10 09:33:19 -0300

    Captar todos los valores de una sesion y mensaje de bienvenida
    ASI DE GRANDE
    ---------------------------------------------------------------------------------------------------------------

--- PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
+++ PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
@@ -1,6 +1,12 @@
 <?php
 session_start(); 
 $permiso = $_SESSION['tipo'];
+$nombre = $_SESSION['nombre'];
+$apellido = $_SESSION['apellido'];
+$correo = $_SESSION['correo'];
+$contraseña = $_SESSION['password'];
+$direccion = $_SESSION['direccion'];
+$telefono = $_SESSION['telefono'];
 
 if($permiso == "cliente"){
     echo "<li><a class='boton' href=''>1OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
@@ -20,6 +26,10 @@ if($permiso == "cliente"){
     echo "<li style='float:left'><a href='GestionDeProductos.php'>Gestor de Productos</a></li>";
     echo "<li style='float:left'><a href=''>3OpcionAdministrador</a></li>";
     echo "<li style='float:left'><a href=''>4OpcionAdministrador</a></li>"; */
+    if($_SESSION['nuevo_ingreso'] == "true"){
+    echo "Bienvenido ".$nombre. "!";
+    $_SESSION['nuevo_ingreso'] = "false";
+    }
     echo "<li><a class='boton' href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href='GestionDeProductos.php'>Gestor de Productos</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>3OpcionAdministrador</a></li><hr size=5 noshade='noshade' color='#000'>";
