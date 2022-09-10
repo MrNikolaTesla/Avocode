68a181fd079b820b99010b585ac7757860740173
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-09-10 09:46:07 -0300

    Update MenuPrincipal_VerificacionPermisos.php

--- PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
+++ PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
@@ -14,8 +14,6 @@ if($permiso == "cliente"){
     echo "<li><a class='boton' href=''>3OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>4OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href='../controlador/Logout.php'>Logout</a></li><hr size=5 noshade='noshade' color='#000'>";
-    
-
 
  }else if ($permiso == "empleado"){
     echo "<li><a class='boton' href=''>1OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
@@ -23,6 +21,7 @@ if($permiso == "cliente"){
     echo "<li><a class='boton' href=''>3OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>4OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href='../controlador/Logout.php'>Logout</a></li><hr size=5 noshade='noshade' color='#000'>";
+
 }else{
     /* echo "<li style='float:left'><a href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li>";
     echo "<li style='float:left'><a href='GestionDeProductos.php'>Gestor de Productos</a></li>";
