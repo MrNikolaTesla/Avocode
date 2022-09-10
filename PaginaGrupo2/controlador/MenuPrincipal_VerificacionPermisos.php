5e52421ed251aeaa1d3b82974a5c51c8f0e8fcc4
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-09-10 09:41:44 -0300

    Agregado Logout a los otros dos tipos de usuarios

--- PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
+++ PaginaGrupo2/controlador/MenuPrincipal_VerificacionPermisos.php
@@ -13,6 +13,7 @@ if($permiso == "cliente"){
     echo "<li><a class='boton' href=''>2OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>3OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>4OpcionCliente</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href='../controlador/Logout.php'>Logout</a></li><hr size=5 noshade='noshade' color='#000'>";
     
 
 
@@ -21,6 +22,7 @@ if($permiso == "cliente"){
     echo "<li><a class='boton' href=''>2OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>3OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
     echo "<li><a class='boton' href=''>4OpcionEmpleado</a></li><hr size=5 noshade='noshade' color='#000'>";
+    echo "<li><a class='boton' href='../controlador/Logout.php'>Logout</a></li><hr size=5 noshade='noshade' color='#000'>";
 }else{
     /* echo "<li style='float:left'><a href='GestionDeUsuarios.php'>Gestor de Usuarios</a></li>";
     echo "<li style='float:left'><a href='GestionDeProductos.php'>Gestor de Productos</a></li>";
