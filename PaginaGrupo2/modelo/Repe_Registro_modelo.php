ff815bacbc86ddff9ecb11aba7d07efdac4e5316
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-09-10 16:36:04 +0200

    Update Repe_Registro_modelo.php

--- PaginaGrupo2/modelo/Repe_Registro_modelo.php
+++ PaginaGrupo2/modelo/Repe_Registro_modelo.php
@@ -10,9 +10,9 @@ class Repe_Registro_modelo
         $this->con = conectar::conexion();
     }
 
-    public function get_correo($correo)
+    public function get_usuario($nombre, $apellido, $correo, $password, $direccion, $telefono)
     {
-        $sql = "SELECT * FROM usuario WHERE correo = '$correo'";
+        $sql = "SELECT * FROM usuario WHERE nombre LIKE '%$nombre%' and WHERE apellido LIKE '%$apellido%' and WHERE correo = '$correo' and WHERE password = '$password' and WHERE direccion = '$direccion' and WHERE telefono = 'telefono'";
         $query = mysqli_query($this->con, $sql);
         $result = mysqli_fetch_array($query);
         return $result;
