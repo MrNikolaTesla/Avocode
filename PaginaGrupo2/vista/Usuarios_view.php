ff56198c919853e208f606f0fc17d61c6ce14c8f
Lautaro <110629515+MrNikolaTesla@users.noreply.github.com>
2022-09-14 10:05:12 -0300

    Rediseño de tablas
    Tablas arregladas, falta posicionar y el título

--- PaginaGrupo2/vista/Usuarios_view.php
+++ PaginaGrupo2/vista/Usuarios_view.php
@@ -14,52 +14,49 @@
 
 <body>
     <?php
-
     foreach ($matrizUsuario as $registro) {
-
         //echo $registro['matricula'] . " - " . $registro['nombre']. " <br> " ;
     }
     ?>
-    <div class="container-fluid">
-        <!-- No se cual es esta class porque es del boostrap -->
-        <!-- INICIO FORMULARIO ALTA -->
-        <form class="col-4" method="POST">
+    <!-- No se cual es esta class porque es del boostrap -->
+    <!-- INICIO FORMULARIO ALTA -->
 
-            <h3 class="text-center2">Tabla de usuarios</h3>
-            <table class="tablas-usuarios">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Tipo de Usuario</th>
-                        <th>Correo</th>
-                        <th>Telefono</th>
-                        <th>Direccion</th>
-                        <th colspan="2">Contraseña</th>
-                        <th colspan="2">Eliminar</th>
-                        <th colspan="2">Modificar</th>
-                    <tr>
-                </thead>
-    </div>
-    </form>
-    <tbody>
-
-        <?php foreach ($matrizUsuario as $usuario) : ?>
+    <table id="customers">
+        <thead>
             <tr>
-                <td><?php echo $usuario['id_usuario'] ?></td>
-                <td><?php echo $usuario['nombre'] ?></td>
-                <td><?php echo $usuario['apellido'] ?></td>
-                <td><?php echo $usuario['tipo'] ?></td>
-                <td><?php echo $usuario['correo'] ?></td>
-                <td><?php echo $usuario['telefono'] ?></td>
-                <td colspan="2"><?php echo $usuario['direccion'] ?></td>
-                <td colspan="2"><?php echo $usuario['password'] ?></td>
-                <td colspan="2"><a href="../controlador/eliminar_Usuario_controlador.php?id=<?php echo $usuario['id_usuario'] ?>" class="btn btn-danger"> Eliminar</a></td>
-                <td colspan="2"><a href="ModificarUsuario_pagina.php?id=<?php echo $usuario['id_usuario'] ?>" class="btn btn-secondary">Modificar</a></td>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Tipo de Usuario</th>
+                <th>Correo</th>
+                <th>Teléfono</th>
+                <th>Dirección</th>
+                <th>Contraseña</th>
+                <th>Eliminar</th>
+                <th>Modificar</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
+        </thead>
+
+        </div>
+        </form>
+
+        <h3 class="text-center">Modificacion de Usuarios</h3>
+        <tbody>
+            <?php foreach ($matrizUsuario as $usuario) : ?>
+                <tr>
+                    <td><?php echo $usuario['id_usuario'] ?></td>
+                    <td><?php echo $usuario['nombre'] ?></td>
+                    <td><?php echo $usuario['apellido'] ?></td>
+                    <td><?php echo $usuario['tipo'] ?></td>
+                    <td><?php echo $usuario['correo'] ?></td>
+                    <td><?php echo $usuario['telefono'] ?></td>
+                    <td><?php echo $usuario['direccion'] ?></td>
+                    <td><?php echo $usuario['password'] ?></td>
+                    <td><a href="../controlador/eliminar_Usuario_controlador.php?id=<?php echo $usuario['id_usuario'] ?>" class="btn btn-danger"> Eliminar</a></td>
+                    <td><a href="ModificarUsuario_pagina.php?id=<?php echo $usuario['id_usuario'] ?>" class="btn btn-secondary">Modificar</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
 
 </body>
