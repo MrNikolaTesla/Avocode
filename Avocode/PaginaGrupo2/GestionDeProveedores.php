2814bd6eb6e663ae2b4b41f5728db73e287a4203
Lautaro <galeazzil2021@gmail.com>
2022-10-11 19:52:40 -0300

    commit botones

--- Avocode/PaginaGrupo2/GestionDeProveedores.php
+++ Avocode/PaginaGrupo2/GestionDeProveedores.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Gestor de Proveedores</title>
         <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/styles2.css">
-
+        <title>Usuario</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <!-- Recursos Misceláneos (Bootstrap CSS, Tipografías, CSS variado) -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <!-- JQUERY-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- BOOTSTRAP JAVASCRIPT -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
--- Avocode/PaginaGrupo2/vista/buscar_proveedor_view.php
+++ Avocode/PaginaGrupo2/vista/buscar_proveedor_view.php
@@ -1,15 +1,29 @@
-<div class="col-xs-4">
-<form method="POST">
-                            <div class="search-box">
-                            <?php require_once("controlador/Controlador_Buscar_Proveedor.php");?>
-                                <div class="input-group" type="text" name="busqueda">
-                                    <span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
-                                    <input type="text" name="proveedor" class="form-control" placeholder="Buscar&hellip;">
-                                    <input class="buttons" type="submit" name="busqueda" value="Buscar"></input>
-                                    <input class="buttons" type="submit" value="Reiniciar"></input>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </form>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busqueda de Proveedor</title>
+</head>
+
+<body>
+    <div class="col-xs-4">
+        <form method="POST">
+            <div class="search-box">
+                <?php require_once("controlador/Controlador_Buscar_Proveedor.php"); ?>
+                <div class="input-group" type="text" name="busqueda">
+                    <span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
+                    <input type="text" name="proveedor" class="form-control" placeholder="Buscar&hellip;">
+                    <input hidden class="buttons" type="submit" name="busqueda" value="Buscar"></input>
                 </div>
+            </div>
+    </div>
+    <div><input class="button-3" type="submit" value="Reiniciar búsqueda"></input> </div>
+    </div>
+    </form>
+    </div>
+</body>
+
+</html>
