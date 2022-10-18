8128ce82764a7063318866af9bef79f14b4949a4
Martín <mexacraftt200@gmail.com>
2022-10-18 16:47:46 -0300

    Eliminar Cuenta

---
+++ Avocode/PaginaGrupo2/controlador/EliminarPerfil_controlador.php
@@ -0,0 +1,12 @@
+<?php
+require_once("../modelo/Usuario.php");
+    $usuario = new Usuario();
+    session_start();
+    if(isset($_GET['id'])){
+
+        $id = $_GET['id'];
+        $usuario->eliminar_cuenta($id);
+
+    header("Location: ../index.php");
+}
+?>
