bc7c2bed9d4fa6eb65a173f4f0dc89acc275e0e3
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-10-18 21:04:02 -0200

    Update eliminar_Usuario_controlador.php

--- Avocode/PaginaGrupo2/controlador/eliminar_Usuario_controlador.php
+++ Avocode/PaginaGrupo2/controlador/eliminar_Usuario_controlador.php
@@ -1,26 +1,22 @@
 <?php
 require_once("../modelo/Usuario.php");
     $usuario = new Usuario();
+    session_start();
     if(isset($_GET['id'])){
 
         $id = $_GET['id'];
         $estado = $usuario->eliminar_usuario($id);
 
 if($estado== 1) {
-    session_start(); 
     $_SESSION['message'] = 'Usuario eliminado correctamente';
     header("Location: ../PAGINA_GestionUsuarios.php");
 }else if($estado == 2){
-    session_start(); 
     $_SESSION['message'] = 'No puedes eliminarte a ti mismo!';
     header("Location: ../PAGINA_GestionUsuarios.php");
 }else if($estado==0){
-    session_start(); 
     $_SESSION['message'] = 'No tienes los permisos para realizar esta accion.';
     header("Location: ../PAGINA_GestionUsuarios.php");
-    
 }else{
-    session_start(); 
     $_SESSION['message'] = 'El usuario seleccionado no existe.';
     header("Location: ../PAGINA_GestionUsuarios.php");
 }
