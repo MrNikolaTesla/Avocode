cc4bf49886ce63feddbc5ae033eed764a878d05f
Lautaro <galeazzil2021@gmail.com>
2022-10-19 11:11:53 -0300

    boton arreglado

--- Avocode/PaginaGrupo2/vista/Modificar_perfil_view.php
+++ Avocode/PaginaGrupo2/vista/Modificar_perfil_view.php
@@ -5,6 +5,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="assets/styles2.css">
+
         <title>Modificacion de Usuario</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <!-- Recursos Misceláneos (Bootstrap CSS, Tipografías, CSS variado) -->
@@ -49,7 +51,7 @@
                         </div>
                         <a href="menu_principal.php" class="button-2" style="color:white; text-decoration:none;">Regresar</a>
                         <button type="submit" class="button-3" name="modificacion" value="enviar">Modificar</button>
-                        <a href="controlador/EliminarPerfil_controlador.php?id=<?php echo $id_perfil ?>"><button type="submit" class="button-4">Eliminar cuenta</a></button>
+                        <a href="controlador/EliminarPerfil_controlador.php?id=<?php echo $id_perfil ?>" class="button-4" style="color:white; text-decoration:none;">Eliminar cuenta</a></button>
                 </form>
         <?php }else{  ?>
                 <div class="centrado">No estas modificando tu propio perfil...</div>
