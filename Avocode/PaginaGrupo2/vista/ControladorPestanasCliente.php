a2ec08f368700b4cb4b2775901e1dc207cd15473
Tabare8031 <facundovuerra@gmail.com>
2022-10-23 01:44:16 -0200

    Update ControladorPestanasCliente.php

--- Avocode/PaginaGrupo2/vista/ControladorPestanasCliente.php
+++ Avocode/PaginaGrupo2/vista/ControladorPestanasCliente.php
@@ -4,6 +4,7 @@ if (isset($_SESSION['sesion_iniciada']) && $_SESSION['sesion_iniciada'] == "true
     <li><a href="#" class="active">Inicio</a></li>
     <li><a href="#">Cartilla</a></li>
     <li><a href="#">Edición Limitada</a></li>
+    <li><a href="#">Editar Perfil</a></li>
     <li><a href="#">Sobre nosotros</a></li>
     <li><a href="#">Contacto</a></li>
     <li><a href='controlador/Logout.php'>Logout</a></li>
