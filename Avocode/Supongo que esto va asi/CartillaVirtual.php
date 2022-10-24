c7b63d32647931e77896ad0c9dd7486e4bc61b17
Martín <mexacraftt200@gmail.com>
2022-10-24 15:36:40 -0300

    Te juro que lo comprendo

--- Avocode/Supongo que esto va asi/CartillaVirtual.php
+++ Avocode/Supongo que esto va asi/CartillaVirtual.php
@@ -1,5 +1,7 @@
 <!--Llamar al controlador para traer la cartilla-->
 
-<!--Llamar al controlador para mostrar los productos dentro de tu orden siempre que su estado sea "Generandose..."-->
+<!--Boton para empezar a crear una orden (view)-->
+
+<!--Llamar al controlador para mostrar los productos dentro de tu orden siempre que su estado sea "Generandose..." y haya una orden, caso contrario que no haga NADA-->
 
 <!--Puedo verificar si cambias el id y regresarte a esta pagina por pvto-->
