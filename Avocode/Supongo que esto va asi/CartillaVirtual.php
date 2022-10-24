62d4e3f9cdb94125a03535d898c4cf194455598d
Martín <mexacraftt200@gmail.com>
2022-10-24 15:00:53 -0300

    Tratando de comprender orden

---
+++ Avocode/Supongo que esto va asi/CartillaVirtual.php
@@ -0,0 +1,3 @@
+<!--Llamar al controlador para traer la cartilla-->
+
+<!--Llamar al controlador para mostrar los productos dentro de tu orden-->
---
+++ Avocode/Supongo que esto va asi/controlador/ControladorCartillaVirtual.php
@@ -0,0 +1,5 @@
+<!--Llamada al modelo -->
+
+<!--Probablemente necesite un listado de los productos-->
+
+<!--Llamar la view-->
