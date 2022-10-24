2396054a0a77837d713452fd91882ac0fa14b2ef
Martín <mexacraftt200@gmail.com>
2022-10-24 18:18:56 -0300

    Update BotonesParaAgregarAOrden_view.php

--- Avocode/PaginaGrupo2/vista/BotonesParaAgregarAOrden_view.php
+++ Avocode/PaginaGrupo2/vista/BotonesParaAgregarAOrden_view.php
@@ -2,6 +2,6 @@
     <!--Cantidad del mismo producto-->
     <input type="number"></input>
     <!--Este boton permitiria agregar un producto a la orden, probablemente vaya a tener un href que se lleve la id del producto, que incluso sin estar mostrada la tenemos detras-->
-    <button>IDK!</button>
+    <a href="controlador/Controlador_Agregar_A_Orden.php?id=<?php echo $producto['id_producto'] ?>"><button>IDK!</button></a>
 
 </td>
