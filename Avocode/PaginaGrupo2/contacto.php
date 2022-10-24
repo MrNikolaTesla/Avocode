cdf7752d28e8831af7be75b9723a010a6f6ca8d4
sebamestoy <110848032+sebamestoy@users.noreply.github.com>
2022-10-24 20:18:45 -0300

    contacto

--- Avocode/PaginaGrupo2/contacto.php
+++ Avocode/PaginaGrupo2/contacto.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html lang="es">
+<link rel="stylesheet" type="text/css" href="assets/styles_inicio.css">
 
 <head>
   <title>Contactos</title>
+  <h1> Redes Sociales</h1>
 </head>
 
 <body>
-<div> ahorita nomas  </div>
-<br><a href="index.php">Regresar</a>   
+<p> Mantente al tanto y comunicate con nosotros atraves de nuestras redes sociales  </p>
 </body>
 
+        <a href="https://api.whatsapp.com/send?phone=59891646540">
+          <img width="70" height="70" src="assets/imagenes/whatsapp.png" alt="whatsapp"> </a>
+  
+        <a href="https://www.instagram.com/burgerfactorym/">
+          <img width="70" height="70" src="assets/imagenes/instagram.png" alt="instagram"> </a>
+
+        <a href="https://www.facebook.com/burgerfactorymaldonado">
+          <img width="70" height="70" src="assets/imagenes/facebook.png" alt="facebook"> </a>
+     
+
+      <br><a href="index.php">Regresar</a>  
 </html>
