1874b93230aaf84e58516e9c08c745e8410596ed
Martín <77687435+LatiosLaw@users.noreply.github.com>
2022-10-25 15:49:42 -0200

    Update VistaPestanasCliente.php

---
+++ Avocode/PaginaGrupo2/vista/VistaPestanasCliente.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+    if (isset($_SESSION['sesion_iniciada']) && $_SESSION['sesion_iniciada'] == "true") { ?>
+        <li class="top-bar"><a href="PAGINA_CartillaVirtual.php">Cartilla Virtual</a></li>
+        <li class="top-bar"><a href="PAGINA_MisOrdenes.php">Mis Ordenes</a></li>
+        <li class="top-bar"><a href="ModificarPerfilCliente_pagina.php?id=<?php echo $_SESSION['id'] ?>">Perfil</a></li>
+        <li class="top-bar"><a href='controlador/Logout.php'>Logout</a></li>
+    <?php } else { ?>
+        <li class="top-bar"><a href="PAGINA_CartillaVirtual.php">Cartilla Virtual</a></li>
+        <li class="top-bar"><a href="FormularioInicioSesion.php">Iniciar Sesión</a></li>
+        <li class="top-bar"><a href="FormularioRegistroCliente.php">Registrar</a></li>
+    <?php
+    }
+?>
