3e5a99f5bd96280fa82e390b5cf95f78ded86228
sebamestoy <110848032+sebamestoy@users.noreply.github.com>
2022-10-25 16:14:43 -0300

    Sobre Nosotros

--- Avocode/PaginaGrupo2/sobre-nosotros.php
+++ Avocode/PaginaGrupo2/sobre-nosotros.php
@@ -2,6 +2,8 @@
 <html lang="es">
 
 <head>
+<link rel="stylesheet" type="text/css" href="assets/styles_sobre_nosostros.css">
+
   <title>Sobre Nosotros</title>
 </div>
   <h1> Burger Factory</h1>
@@ -10,15 +12,17 @@
 
 <body>
 </div>
-  <h3> Sobre Nosotros</h3>
+<section class="form-register">
+  <h2> Sobre Nosotros</h2>
   </div>
-<div> <h5>Somos un establecimiento gastronómico con un menú enfocado en las hamburguesas y sus acompañamientos. Dentro de nuestro menú tenemos diferentes tipos de hamburguesas las cuales incluimos son: hamburguesas clásica, Burger Gringa, Burger Cabrona, Burger Champi , Burger Red Hot, Burger Super Cheese, Sanderburger.
+<div> <h3>Somos un establecimiento gastronómico con un menú enfocado en las hamburguesas y sus acompañamientos. Dentro de nuestro menú tenemos diferentes tipos de hamburguesas las cuales incluimos son: hamburguesas clásica, Burger Gringa, Burger Cabrona, Burger Champi , Burger Red Hot, Burger Super Cheese, Sanderburger.
 Aparte de las clásicas hamburguesas de carne tenemos hamburguesas vegetarianas : Burger Lentejas y Burger Blue Cheese.
-Además tiene una sección de ensaladas, refrescos y cervezas. Todas las hamburguesas tienen incluido en el precio papas rusticas.</h5>
+Además tiene una sección de ensaladas, refrescos y cervezas. Todas las hamburguesas tienen incluido en el precio papas rusticas.</h3>
   </div>
+</section>
  
 </body>
-
-<br><a href="index.php">Regresar</a> 
-
+<div>
+<br><a class="button-1" href="index.php">Regresar</a><br>
+</div>
 </html>
