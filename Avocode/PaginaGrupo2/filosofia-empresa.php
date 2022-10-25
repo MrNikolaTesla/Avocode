f270466e0b2365a93b19c5b444086b483019efa4
sebamestoy <110848032+sebamestoy@users.noreply.github.com>
2022-10-25 16:44:36 -0300

    si

--- Avocode/PaginaGrupo2/filosofia-empresa.php
+++ Avocode/PaginaGrupo2/filosofia-empresa.php
@@ -2,12 +2,31 @@
 <html lang="es">
 
 <head>
-  <title>Contactos</title>
+<link rel="stylesheet" type="text/css" href="assets/styles_filosofia-empresa.css">
+
+  <title>Filosofia de la Empresa</title>
+</div>
+  <h1> Burger Factory</h1>
+  </div>
 </head>
 
 <body>
-<div> ahorita nomas  </div>
-<br><a href="index.php">Regresar</a>   
+</div>
+<section class="form-register">
+  <h2> Nuestra Filosofia</h2>
+  </div>
+<div> <h3>blablablablablablablablablablablablablablablablabb
+lablablablablablablablablablablablablablablablablabla
+lablablablablablablablablablablablablablablablablabla
+lablablablablablablablablablablablablablablablablabla
+lablablablablablablablablablablablablablablablablabla
+lablablablablablablablablablablablablablablablablabla.
+</h3>
+  </div>
+</section>
+ 
 </body>
-
+<div>
+<br><a class="button-1" href="index.php">Regresar</a><br>
+</div>
 </html>
