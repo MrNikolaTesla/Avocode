f84f08e63305219f7873801dbb2f6a95778f2ca0
sebamestoy <110848032+sebamestoy@users.noreply.github.com>
2022-10-24 21:37:37 -0300

    o ma gaaa

--- Avocode/PaginaGrupo2/contacto.php
+++ Avocode/PaginaGrupo2/contacto.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
-<link rel="stylesheet" type="text/css" href="assets/styles_inicio.css">
 
 <head>
+<link rel="stylesheet" type="text/css" href="assets/styleContact.css">
   <title>Contactos</title>
+  </div>
   <h1> Redes Sociales</h1>
+  </div>
 </head>
-
-<body>
+<body> 
+  <div>
 <p> Mantente al tanto y comunicate con nosotros atraves de nuestras redes sociales  </p>
-</body>
+</div>
 
+</body>
+<div class="logos"  >
+        <div class="logo" id="logo1" >
         <a href="https://api.whatsapp.com/send?phone=59891646540">
           <img width="70" height="70" src="assets/imagenes/whatsapp.png" alt="whatsapp"> </a>
-  
+          </div>
+          <div class="logo" id="logo2" >
         <a href="https://www.instagram.com/burgerfactorym/">
           <img width="70" height="70" src="assets/imagenes/instagram.png" alt="instagram"> </a>
-
+          </div>
+          <div class="logo" id="logo3" >
         <a href="https://www.facebook.com/burgerfactorymaldonado">
           <img width="70" height="70" src="assets/imagenes/facebook.png" alt="facebook"> </a>
-     
+          </div>
+      </div>   
 
-      <br><a href="index.php">Regresar</a>  
+
+
 </html>
