d89e452c08a39777be40b6d1723d59133face44b
Lautaro <110629515+MrNikolaTesla@users.noreply.github.com>
2022-10-25 17:01:30 -0200

    casi me muero

--- Avocode/PaginaGrupo2/vista/mensaje_gestor.php
+++ Avocode/PaginaGrupo2/vista/mensaje_gestor.php
@@ -1,32 +1,38 @@
-<link rel="stylesheet" href="assets/styles_alertas.css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="es">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles_alertas.css">
+    <title>Mensaje Gestor</title>
+    <script>
+        $("#MyButton").click(function() {
+            alert('Confirm to refresh alert messages.');
+            $("#refreshDivID").load("#refreshDivID .reloaded-divs > *");
+        });
+    </script>
+</head>
 
-
-<script>
-    $("#MyButton").click(function() {
-        alert('Confirm to refresh alert messages.');
-        $("#refreshDivID").load("#refreshDivID .reloaded-divs > *");
-    });
-</script>
-
-<?php session_start();
-if (isset($_SESSION['message']) && $_SESSION['message'] != "no") { ?>
-    <div class="container-alert">
-        <br>
-        <div class="col-md-12-1">
-            <div class="alert alert-info alert-dismissable">
-                <div class="alert-icon">
-                    <span class="fa fa-info my_appended_icon"></span>
+<body>
+    <?php session_start();
+    if (isset($_SESSION['message']) && $_SESSION['message'] != "no") { ?>
+        <div class="container-alert">
+            <br>
+            <div class="col-md-12-1">
+                <div class="alert alert-info alert-dismissable">
+                    <div class="alert-icon">
+                        <span class="fa fa-info my_appended_icon"></span>
+                    </div>
+                    <div class="alert-text"> <?= $_SESSION['message'] ?> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    </div>
+                    </button>
                 </div>
-                <div class="alert-text"> <?= $_SESSION['message'] ?> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                </div>
-                </button>
             </div>
         </div>
-    </div>
-<?php $_SESSION['message'] = 'no';
-} ?>
+    <?php $_SESSION['message'] = 'no';
+    } ?>
+</body>
+
+</html>
