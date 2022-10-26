e7add9cf8dad62ac35efbbb1e2aa555c3dda46ab
sebamestoy <110848032+sebamestoy@users.noreply.github.com>
2022-10-26 05:59:54 -0200

    Update sobre-nosotros.php

--- Avocode/PaginaGrupo2/sobre-nosotros.php
+++ Avocode/PaginaGrupo2/sobre-nosotros.php
@@ -12,11 +12,11 @@
 </div>
   <h3> Sobre Nosotros</h3>
   </div>
-<div> Somos un establecimiento gastronómico con un menú enfocado en las hamburguesas y sus acompañamientos. Dentro de nuestro menú tenemos diferentes tipos de hamburguesas las cuales incluimos son: hamburguesas clásica, Burger Gringa, Burger Cabrona, Burger Champi , Burger Red Hot, Burger Super Cheese, Sanderburger.
+<div> <h5>Somos un establecimiento gastronómico con un menú enfocado en las hamburguesas y sus acompañamientos. Dentro de nuestro menú tenemos diferentes tipos de hamburguesas las cuales incluimos son: hamburguesas clásica, Burger Gringa, Burger Cabrona, Burger Champi , Burger Red Hot, Burger Super Cheese, Sanderburger.
 Aparte de las clásicas hamburguesas de carne tenemos hamburguesas vegetarianas : Burger Lentejas y Burger Blue Cheese.
+Además tiene una sección de ensaladas, refrescos y cervezas. Todas las hamburguesas tienen incluido en el precio papas rusticas.</h5>
   </div>
  
-
 </body>
 
 <br><a href="index.php">Regresar</a> 
