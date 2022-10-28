fd03ad1893a9cd3822485a7f06725fdfca61eeff
Martín <mexacraftt200@gmail.com>
2022-10-28 17:04:08 -0300

    Explicacion de como funciona el sistema para el cliente en cartilla

--- Avocode/PaginaGrupo2/controlador/BtnVerMiOrden_Controlador.php
+++ Avocode/PaginaGrupo2/controlador/BtnVerMiOrden_Controlador.php
@@ -1,4 +1,4 @@
 <?php session_start();
 if(isset($_SESSION['id_orden_actual'])){
-   ?> <br><a href="#orden">Ver mi Orden</a><br> <?php
+   ?> <br><div class="centrado"><a href="#orden">Ver mi Orden</a></div><?php
 }?>
