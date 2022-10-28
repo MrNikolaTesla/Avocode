5454eb8ddf67fe3cea24ee469f949083a446fcc8
Lautaro <galeazzil2021@gmail.com>
2022-10-28 13:55:25 -0300

    MESAS_VIEW FINAL BOSS
    ==[ ¡MESAS_VIEW HAS BEEN DEFEATED! ]==

--- Avocode/PaginaGrupo2/controlador/Mesa_controlador.php
+++ Avocode/PaginaGrupo2/controlador/Mesa_controlador.php
@@ -9,7 +9,7 @@ $matrizMesa = $mesas->listar_mesas();
 if($matrizMesa){
     require_once("vista/Mesa_view.php");
     }else{
-        ?><div class="no_hay_registros"><?php echo "No hay mesas registradas en el sistema";?></div><?php
+        ?><div><div class="no_hay_registros"><?php echo "No hay mesas registradas en el sistema";?></div></div><?php
 }
 
 ?>
--- Avocode/PaginaGrupo2/vista/RegistroDeMesas.php
+++ Avocode/PaginaGrupo2/vista/RegistroDeMesas.php
@@ -5,7 +5,15 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/styles2.css">
         <title>Registro de Mesas</title>
+        <!-- Recursos Misceláneos (Bootstrap CSS, Tipografías, CSS variado) -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <!-- JQUERY-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- BOOTSTRAP JAVASCRIPT -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
