25ec45b91762d7f97541798e911c2fa62de82aa4
Martín <mexacraftt200@gmail.com>
2022-10-28 17:23:36 -0300

    Orden Local

--- Avocode/PaginaGrupo2/controlador/ControladorProcesarOrden.php
+++ Avocode/PaginaGrupo2/controlador/ControladorProcesarOrden.php
@@ -1,24 +1,39 @@
 <?php
 require_once("modelo/Orden.php");
 $orden = new Orden();
+require_once("modelo/Mesa.php");
+    $mesa = new Mesa();
 if (!empty($_POST["completar"])) {
     if($_SESSION['tipo_orden'] == "local"){
         if (!empty($_POST["num_mesa"])) {
 
         $id_orden = $_SESSION['id_orden_actual'];
-        $mesa = $_POST["num_mesa"];
+        $num_mesa = $_POST["num_mesa"];
         $tipo_orden = "Local";
         $observacion = $_POST["observacion"];
         $estado_orden = "Pendiente";
 
-        $resultado = $orden->update_orden_local($id_orden, $mesa, $tipo_orden, $observacion, $estado_orden);
+        $estado_mesa = $mesa->get_estado_mesa($num_mesa);
 
+            foreach($estado_mesa as $mesa_elegida){
+                $confirmacion_estado = $mesa_elegida['estado'];
+            }
+        
+
+        if($confirmacion_estado != "Libre"){
+            $resultado=2;
+        }else{
+            $resultado = $orden->update_orden_local($id_orden, $mesa, $tipo_orden, $observacion, $estado_orden);
+        }
+    
         if($resultado){
             require_once("modelo/Mesa.php");
             $mesa_a_ocupar = new Mesa();
             $mesa_a_ocupar->ocupar_mesa($mesa);
             $_SESSION['completando_orden'] = "false";
             require_once("Controlador_CompletarOrden.php");
+        }else if($resultado==2){
+
         }else{
             echo "Algo ha fallado, intentelo de nuevo mas tarde.";
         }
