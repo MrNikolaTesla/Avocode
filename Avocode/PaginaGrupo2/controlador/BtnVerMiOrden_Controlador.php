532e3adb543b5aabfe7292c3333ea63f2a037485
Lautaro <110629515+MrNikolaTesla@users.noreply.github.com>
2022-10-29 12:51:32 +0200

    TODO ESTO PARA UN BOTON
    Mainentti Sexo

--- Avocode/PaginaGrupo2/controlador/BtnVerMiOrden_Controlador.php
+++ Avocode/PaginaGrupo2/controlador/BtnVerMiOrden_Controlador.php
@@ -1,4 +1,4 @@
 <?php session_start();
-if(isset($_SESSION['id_orden_actual'])){
-   ?> <br><div class="centrado"><a href="#orden">Ver mi Orden</a></div><?php
-}?>
+if (isset($_SESSION['id_orden_actual'])) {
+?><div class="centrado"><a href="#orden"><button class="button-1-cartilla" style="text-decoration:none" color="white" role="button">Ver mi Orden</button></a></div><?php
+                                                                                                                                    } ?>
--- Avocode/PaginaGrupo2/vista/Productos_de_Orden_view.php
+++ Avocode/PaginaGrupo2/vista/Productos_de_Orden_view.php
@@ -1,37 +1,38 @@
  <!-- DIV'S CON CLASES DIFERENTES PARA EL RESPONSIVE Y DISPOSICIÓN EN LA PÁGINA -->
-                            <!------------------------------------------------------------------------------->
+ <!------------------------------------------------------------------------------->
 
-                        <!-- COMIENZO DEL FORMULARIO DE PROVEEDORES -->
-                        <h3 class="centrado">Tu Orden :</h3>
-                        <form method="POST">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Nombre</th>
-                                        <th>Cantidad</th>
-                                        <th>Precio Unitario</th>
-                                        <th>Precio Total</th>
-                                        <th>Eliminar</th>
-                                    </tr>
-                                </thead>
-                        </form>
+ <!-- COMIENZO DEL FORMULARIO DE PROVEEDORES -->
+ <h3 class="centrado">Tu Orden :</h3>
+ <form method="POST">
+     <table class="table table-bordered">
+         <thead>
+             <tr>
+                 <th>Nombre</th>
+                 <th>Cantidad</th>
+                 <th>Precio Unitario</th>
+                 <th>Precio Total</th>
+                 <th>Eliminar</th>
+             </tr>
+         </thead>
+ </form>
 
-                        <!-- CUERPO DE TABLA -->
-                        <tbody>
-                        <?php $precio_total = 0; foreach ($matrizProdOrden as $detalle) : ?>
-                                <tr>
-                                    <td><?php echo $detalle['nombre'] ?></td>
-                                    <td><?php echo $detalle['cantidad'] ?></td>
-                                    <td><?php echo $detalle['precio_linea'] ?></td>
-                                    <td><?php $precio_total = $precio_total + $detalle['total_linea'];
-                                    echo $detalle['total_linea'] ?></td>
-                                    <td>
-                                        <a href="controlador/eliminar_LineaOrden.php?id=<?php echo $detalle['identificador_detalle'] ?>" class="delete" title="Eliminar" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                                    </td>
-                                </tr>
-                                <?php endforeach; ?>
-                        </tbody>
-                        </table>
-                        <div class="centrado"><b>Total : $<?php echo $precio_total; ?></b></div>
-                        <div class="centrado"><a class="button-2" href="PAGINA_ContinuarOrden.php" style="color:white;">Continuar</a></div>
-                        <div class="centrado"><a class="button-3" href="controlador/Controlador_CancelarOrden.php" style="color:white;">Cancelar Orden</a></div>
+ <!-- CUERPO DE TABLA -->
+ <tbody>
+     <?php $precio_total = 0;
+        foreach ($matrizProdOrden as $detalle) : ?>
+         <tr>
+             <td><?php echo $detalle['nombre'] ?></td>
+             <td><?php echo $detalle['cantidad'] ?></td>
+             <td><?php echo $detalle['precio_linea'] ?></td>
+             <td><?php $precio_total = $precio_total + $detalle['total_linea'];
+                    echo $detalle['total_linea'] ?></td>
+             <td>
+                 <a href="controlador/eliminar_LineaOrden.php?id=<?php echo $detalle['identificador_detalle'] ?>" title="Eliminar"><img src="assets/iconos/icono-papelera.svg"></a>
+             </td>
+         </tr>
+     <?php endforeach; ?>
+ </tbody>
+ </table>
+ <div class="centrado"><b>Total : $<?php echo $precio_total; ?></b></div>
+ <div class="centrado"><a class="button-2" href="PAGINA_ContinuarOrden.php" style="color:white;">Continuar</a></div>
+ <div class="centrado"><a class="button-3" href="controlador/Controlador_CancelarOrden.php" style="color:white;">Cancelar Orden</a></div>
