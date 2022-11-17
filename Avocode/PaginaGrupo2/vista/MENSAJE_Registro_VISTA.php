d75f7629475f4f7e0028b2dfb7aaab115662a205
Lautaro <galeazzil2021@gmail.com>
2022-11-16 21:53:21 -0300

    commit productos y optimizacion

--- Avocode/PaginaGrupo2/vista/MENSAJE_Registro_VISTA.php
+++ Avocode/PaginaGrupo2/vista/MENSAJE_Registro_VISTA.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensaje Gestor</title>
-    <script>
-        $("#MyButton").click(function() {
-            alert('Confirm to refresh alert messages.');
-            $("#refreshDivID").load("#refreshDivID .reloaded-divs > *");
-        });
-    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <!-- JQUERY-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- BOOTSTRAP JAVASCRIPT -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
